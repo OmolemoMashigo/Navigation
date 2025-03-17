b0dfcea3cb1a1bed641a594ff122253c24f68ffa
Omolemo Mashigo <omomashigo@gmail.com>
2025-03-17 21:21:24 +0200

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Omolemo Mashigo on 2025/03/17.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
